dfd7e3f66e6ef5c947254aee8568021044a20578
Mike <michaljot@student.agh.edu.pl>
2020-12-30 11:37:49 +0100

    Merge fix

--- konfigurator_pc/project/app/Models/PCConfig.php
+++ konfigurator_pc/project/app/Models/PCConfig.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class PCConfig extends Model
 {
-    protected $fillable =  ['cpu_id', 'gpu_id', 'mb_id','ram_id'];
     use HasFactory;
 
     protected $table = 'pc_configs';
@@ -25,7 +24,5 @@ class PCConfig extends Model
         'benchmark',
         'price',
         'user_id',
-        'is_verified',
-        'share_url',
     ];
 }
