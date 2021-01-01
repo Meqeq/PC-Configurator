9e371f780e833b03d96ff011dc493cca65722be3
Dawid Jarosz <dawj@op.pl>
2021-01-01 19:27:56 +0100

    Tests for multiple range filter types

--- konfigurator_pc/project/tests_codeception/acceptance/05_ComponentsSelectionCept.php
+++ konfigurator_pc/project/tests_codeception/acceptance/05_ComponentsSelectionCept.php
@@ -71,6 +71,33 @@ $I->click("Filter");
 $I->see("2900", ".frequency");
 $I->dontSee("3600", ".frequency");
 
+// Two ranges filters
+
+$I->amOnPage('/components/cpu/select');
+
+$I->fillField("#price_min", 0);
+$I->fillField("#price_max", 10000);
+$I->fillField("#frequency_min", 1000);
+$I->fillField("#frequency_max", 4000);
+$I->click("Filter");
+
+$I->see("Intel Core i5-9400", '.name');
+$I->see("AMD Ryzen 5 3600", '.name');
+$I->see("Intel Core i5-10400F", ".name");
+
+$I->fillField("#price_min", 720);
+$I->fillField("#price_max", 10000);
+$I->fillField("#frequency_min", 2800);
+$I->fillField("#frequency_max", 3400);
+$I->click("Filter");
+
+$I->see("Intel Core i5-9400", '.name');
+$I->dontSee("AMD Ryzen 5 3600", '.name');
+$I->dontSee("Intel Core i5-10400F", ".name");
+
+
+
+
 
 
 
