d609456805afd8a8b48b15fce21ba0b1e6fc63a1
Kamil Wierciak <kwierciak@student.agh.edu.pl>
2021-01-02 17:21:15 +0100

    refactor

--- konfigurator_pc/project/resources/views/pcconfig/show.blade.php
+++ konfigurator_pc/project/resources/views/pcconfig/show.blade.php
@@ -34,8 +34,8 @@
                             </div>
                         </div>
                         <div class="flex items-center justify-end mt-4">
-                            <a href="/pcconfig/{{$pcConfig->id}}/edit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3">{{ __('Edit') }}</a>
-                            <form action="/pcconfig/{{$pcConfig->id}}" method="POST">
+                            <a href="{{route('pcconfig.edit', $pcConfig->id)}}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3">{{ __('Edit') }}</a>
+                            <form action="{{ route('pcconfig.destroy', $pcconfig->id) }}" method="POST">
                                 @method('DELETE')
                                 @csrf
                                 <input type="hidden" name="_method" value="DELETE">
